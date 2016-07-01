cc63ed751f07ea92efd5d74f697bb288ffca41dd
jodaf <spinaltap12345@gmail.com>
2016-07-01 16:36:10 -0700

    Enter animal companion stats and calculations.

--- Pathfinder.js
+++ Pathfinder.js
@@ -100,28 +100,28 @@ function Pathfinder() {
 
 // Arrays of choices
 Pathfinder.ANIMAL_COMPANIONS = {
-  'Ape': '',
-  'Badger': '',
-  'Bear': '',
-  'Boar': '',
-  'Camel': '',
-  'Cheetah': '',
-  'Constrictor': '',
-  'Crocodile': '',
-  'Deinonychus': '',
-  'Dog': '',
-  'Eagle': '',
-  'Hawk': '',
-  'Horse': '',
-  'Leopard': '',
-  'Lion': '',
-  'Owl': '',
-  'Pony': '',
-  'Shark': '',
-  'Tiger': '',
-  'Velociraptor': '',
-  'Viper': '',
-  'Wolf': ''
+  'Ape': 'AC=11 Dam=2x1d4,1d4 Str=13 Dex=17 Con=10 Int=2 Wis=12 Cha=7',
+  'Badger': 'AC=12 Dam=1d4 Str=10 Dex=17 Con=15 Int=2 Wis=12 Cha=10',
+  'Bear': 'AC=12 Dam=2x1d3,1d4 Str=15 Dex=15 Con=13 Int=2 Wis=12 Cha=6',
+  'Boar': 'AC=16 Dam=1d6 Str=13 Dex=12 Con=15 Int=2 Wis=13 Cha=4',
+  'Camel': 'AC=11 Dam=1d4 Str=18 Dex=16 Con=14 Int=2 Wis=11 Cha=4',
+  'Cheetah': 'AC=11 Dam=2x1d2,1d4 Str=12 Dex=21 Con=13 Int=2 Wis=12 Cha=6',
+  'Constrictor': 'AC=12 Dam=1d3 Str=15 Dex=17 Con=13 Int=1 Wis=12 Cha=2',
+  'Crocodile': 'AC=14 Dam=1d6 Str=15 Dex=14 Con=15 Int=1 Wis=12 Cha=2',
+  'Deinonychus': 'AC=11 Dam=2x1d6,1d4 Str=11 Dex=17 Con=17 Int=2 Wis=12 Cha=14',
+  'Dog': 'AC=12 Dam=1d4 Str=13 Dex=17 Con=15 Int=2 Wis=12 Cha=6',
+  'Eagle': 'AC=11 Dam=2x1d4+1d4 Str=10 Dex=15 Con=12 Int=2 Wis=14 Cha=6',
+  'Hawk': 'AC=11 Dam=2x1d4+1d4 Str=10 Dex=15 Con=12 Int=2 Wis=14 Cha=6',
+  'Horse': 'AC=14 Dam=2x1d6,1d4 Str=16 Dex=13 Con=15 Int=2 Wis=12 Cha=6',
+  'Leopard': 'AC=11 Dam=2x1d2,1d4 Str=12 Dex=21 Con=13 Int=2 Wis=12 Cha=6',
+  'Lion': 'AC=11 Dam=2x1d4,1d6 Str=13 Dex=17 Con=13 Int=2 Wis=15 Cha=10',
+  'Owl': 'AC=11 Dam=2x1d4+1d4 Str=10 Dex=15 Con=12 Int=2 Wis=14 Cha=6',
+  'Pony': 'AC=12 Dam=2x1d3 Str=13 Dex=13 Con=12 Int=2 Wis=11 Cha=4',
+  'Shark': 'AC=14 Dam=1d4 Str=13 Dex=13 Con=12 Int=2 Wis=11 Cha=4',
+  'Tiger': 'AC=11 Dam=2x1d4,1d6 Str=13 Dex=17 Con=13 Int=2 Wis=15 Cha=10',
+  'Velociraptor': 'AC=11 Dam=2x1d6,1d4 Str=11 Dex=17 Con=17 Int=2 Wis=12 Cha=14',
+  'Viper': 'AC=12 Dam=1d3 Str=8 Dex=17 Con=11 Int=1 Wis=12 Cha=2',
+  'Wolf': 'AC=12 Dam=1d6 Str=13 Dex=15 Con=15 Int=2 Wis=12 Cha=6'
 };
 Pathfinder.BLOODLINES = [
   'Aberrant', 'Abyssal', 'Arcane', 'Celestial', 'Destined', 'Draconic',
@@ -159,29 +159,29 @@ Pathfinder.FACTIONS = [
   'The Sczarni', 'The Silver Crusade', 'Taldor'
 ];
 Pathfinder.FAMILIARS = {
-  'Bat': 'HD=1 Init=2 AC=16 Attack=0 Dam=1d3-5 Str=1 Dex=15 Con=6 Int=2 Wis=14 Cha=5',
-  'Cat': 'HD=1 Init=2 AC=14 Attack=0 Dam=2@1d2-4,1d3-4 Str=3 Dex=15 Con=8 Int=2 Wis=12 Cha=7',
-  'Hawk': 'HD=1 Init=3 AC=15 Attack=0 Dam=2@d4-2 Str=6 Dex=17 Con=11 Int=2 Wis=14 Cha=7',
-  'Lizard': 'HD=1 Init=2 AC=14 Attack=0 Dam=1d4-4 Str=3 Dex=15 Con=8 Int=1 Wis=12 Cha=2',
-  'Monkey': 'HD=1 Init=2 AC=14 Attack=0 Dam=1d3-4 Str=3 Dex=15 Con=10 Int=2 Wis=12 Cha=5',
-  'Owl': 'HD=1 Init=3 AC=15 Attack=0 Dam=1d4-2 Str=6 Dex=17 Con=11 Int=2 Wis=15 Cha=6',
-  'Rat': 'HD=1 Init=2 AC=14 Attack=0 Dam=1d3-4 Str=2 Dex=15 Con=11 Int=2 Wis=13 Cha=2',
-  'Raven': 'HD=1 Init=2 AC=14 Attack=0 Dam=1d3-4 Str=2 Dex=15 Con=8 Int=2 Wis=15 Cha=7',
-  'Toad': 'HD=1 Init=1 AC=15 Attack=0 Dam=0 Str=1 Dex=12 Con=6 Int=1 Wis=15 Cha=4',
-  'Viper': 'HD=1 Init=3 AC=16 Attack=0 Dam=1d2-2 Str=4 Dex=17 Con=8 Int=1 Wis=13 Cha=2',
-  'Weasel': 'HD=1 Init=2 AC=15 Attack=0 Dam=1d3-4 Str=3 Dex=15 Con=10 Int=2 Wis=12 Cha=5',
+  'Bat': 'HD=1 AC=16 Dam=1d3-5 Str=1 Dex=15 Con=6 Int=2 Wis=14 Cha=5',
+  'Cat': 'HD=1 AC=14 Dam=2@1d2-4,1d3-4 Str=3 Dex=15 Con=8 Int=2 Wis=12 Cha=7',
+  'Hawk': 'HD=1 AC=15 Dam=2@d4-2 Str=6 Dex=17 Con=11 Int=2 Wis=14 Cha=7',
+  'Lizard': 'HD=1 AC=14 Dam=1d4-4 Str=3 Dex=15 Con=8 Int=1 Wis=12 Cha=2',
+  'Monkey': 'HD=1 AC=14 Dam=1d3-4 Str=3 Dex=15 Con=10 Int=2 Wis=12 Cha=5',
+  'Owl': 'HD=1 AC=15 Dam=1d4-2 Str=6 Dex=17 Con=11 Int=2 Wis=15 Cha=6',
+  'Rat': 'HD=1 AC=14 Dam=1d3-4 Str=2 Dex=15 Con=11 Int=2 Wis=13 Cha=2',
+  'Raven': 'HD=1 AC=14 Dam=1d3-4 Str=2 Dex=15 Con=8 Int=2 Wis=15 Cha=7',
+  'Toad': 'HD=1 AC=15 Dam=0 Str=1 Dex=12 Con=6 Int=1 Wis=15 Cha=4',
+  'Viper': 'HD=1 AC=16 Dam=1d2-2 Str=4 Dex=17 Con=8 Int=1 Wis=13 Cha=2',
+  'Weasel': 'HD=1 AC=15 Dam=1d3-4 Str=3 Dex=15 Con=10 Int=2 Wis=12 Cha=5',
 
-  'Air Elemental': 'HD=2 Init=7 AC=17 Attack=2 Dam=1d4+1 Str=12 Dex=17 Con=12 Int=4 Wis=11 Cha=11 Level=5',
-  'Dire Rat': 'HD=1 Init=3 AC=14 Attack=0 Dam=1d4 Str=10 Dex=17 Con=13 Int=2 Wis=13 Cha=4 Level=3',
-  'Earth Elemental': 'HD=2 Init=-1 AC=17 Attack=2 Dam=1d6+4 Str=16 Dex=8 Con=13 Int=4 Wis=11 Cha=11 Level=5',
-  'Fire Elemental': 'HD=2 Init=5 AC=16 Attack=2 Dam=1d4 Str=10 Dex=13 Con=10 Int=4 Wis=11 Cha=11 Level=5',
-  'Homunculus': 'HD=2 Init=2 AC=14 Attack=2 Dam=1d4-1 Str=8 Dex=15 Con=0 Int=10 Wis=12 Cha=7 Level=7',
-  'Imp': 'HD=3 Init=3 AC=17 Attack=3 Dam=1d4 Str=10 Dex=17 Con=10 Int=13 Wis=12 Cha=14 Level=7',
-  'Mephit': 'HD=3 Init=6 AC=17 Attack=3 Dam=1d3+1 Str=13 Dex=15 Con=12 Int=6 Wis=11 Cha=14 Level=7',
-  'Pseudodragon': 'HD=2 Init=2 AC=16 Attack=2 Dam=1d3-2,1d2-2 Str=7 Dex=15 Con=13 Int=10 Wis=12 Cha=10 Level=7',
-  'Quasit': 'HD=3 Init=6 AC=16 Attack=3 Dam=1d3-1,1d4-1 Str=8 Dex=14 Con=11 Int=11 Wis=12 Cha=11 Level=7',
-  'Stirge': 'HD=1 Init=4 AC=16 Attack=1 Dam=0 Str=3 Dex=19 Con=10 Int=1 Wis=12 Cha=6 Level=5',
-  'Water Elemental': 'HD=2 Init=0 AC=17 Attack=2 Dam=1d6+3 Str=14 Dex=10 Con=13 Int=4 Wis=11 Cha=11 Level=5',
+  'Air Elemental': 'HD=2 AC=17 Dam=1d4+1 Str=12 Dex=17 Con=12 Int=4 Wis=11 Cha=11 Level=5',
+  'Dire Rat': 'HD=1 AC=14 Dam=1d4 Str=10 Dex=17 Con=13 Int=2 Wis=13 Cha=4 Level=3',
+  'Earth Elemental': 'HD=2 AC=17 Dam=1d6+4 Str=16 Dex=8 Con=13 Int=4 Wis=11 Cha=11 Level=5',
+  'Fire Elemental': 'HD=2 AC=16 Dam=1d4 Str=10 Dex=13 Con=10 Int=4 Wis=11 Cha=11 Level=5',
+  'Homunculus': 'HD=2 AC=14 Dam=1d4-1 Str=8 Dex=15 Con=0 Int=10 Wis=12 Cha=7 Level=7',
+  'Imp': 'HD=3 AC=17 Dam=1d4 Str=10 Dex=17 Con=10 Int=13 Wis=12 Cha=14 Level=7',
+  'Mephit': 'HD=3 AC=17 Dam=1d3+1 Str=13 Dex=15 Con=12 Int=6 Wis=11 Cha=14 Level=7',
+  'Pseudodragon': 'HD=2 AC=16 Dam=1d3-2,1d2-2 Str=7 Dex=15 Con=13 Int=10 Wis=12 Cha=10 Level=7',
+  'Quasit': 'HD=3 AC=16 Dam=1d3-1,1d4-1 Str=8 Dex=14 Con=11 Int=11 Wis=12 Cha=11 Level=7',
+  'Stirge': 'HD=1 AC=16 Dam=0 Str=3 Dex=19 Con=10 Int=1 Wis=12 Cha=6 Level=5',
+  'Water Elemental': 'HD=2 AC=17 Dam=1d6+3 Str=14 Dex=10 Con=13 Int=4 Wis=11 Cha=11 Level=5',
 
   'Celestial': 'Level=3',
   'Fiendish': 'Level=3'
@@ -2492,49 +2492,27 @@ Pathfinder.combatRules = function(rules) {
 Pathfinder.companionRules = function(rules, companions, familiars) {
 
   SRD35.companionRules(rules, companions, familiars, null);
-  // Override SRD35 animal companion HD progression
-  rules.defineRule('animalCompanionStats.HD',
-    'animalCompanionMasterLevel', '=', 'source + 1 - Math.floor((source+1) / 4)'
-  );
 
   if(companions != null) {
-
     var notes = [
-      'animalCompanionStats.BAB:+%V',
-      'animalCompanionStats.Fort:+%V',
-      'animalCompanionStats.Ref:+%V',
-      'animalCompanionStats.Will:+%V',
       'animalCompanionStats.Skills:%V',
       'animalCompanionStats.Feats:%V'
     ];
     rules.defineNote(notes);
-
-    rules.defineRule('animalCompanionStats.BAB',
-      'animalCompanionMasterLevel', '=',
-      'Math.floor((source + 2) / 2) + ' +
-      '(source == 9 ? 1 : source < 13 ? 0 : source == 16 ? 0 : 1)'
+    // Overrides SRD35 HD calculation
+    rules.defineRule('animalCompanionStats.HD',
+      'animalCompanionLevel', '=', 'null',
+      'animalCompanionMasterLevel', '=', 'source + 1 - Math.floor((source+1)/4)'
     );
     rules.defineRule('animalCompanionStats.Feats',
       'animalCompanionMasterLevel', '=',
       'source == 18 ? 8 : source >= 10 ? Math.floor((source + 5) / 3) : ' +
       'Math.floor((source + 4) / 3)'
     );
-    rules.defineRule('animalCompanionStats.Fort',
-      'animalCompanionMasterLevel', '=',
-      'Math.floor((source + 8) / 3) + (source > 14 ? 2 : source > 6 ? 1 : 0)'
-    );
-    rules.defineRule('animalCompanionStats.Ref',
-      'animalCompanionMasterLevel', '=',
-      'Math.floor((source + 8) / 3) + (source > 14 ? 2 : source > 6 ? 1 : 0)'
-    );
-    rules.defineRule('animalCompanionStats.Will',
-      'animalCompanionMasterLevel', '=', 'Math.floor((source + 2) / 4)'
-    );
     rules.defineRule('animalCompanionStats.Skills',
       'animalCompanionMasterLevel', '=',
       'source + 1 - Math.floor((source + 1) / 4)'
     );
-
   }
 
   if(familiars != null) {
