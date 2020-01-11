4851c2672f3d16987d839fbf2e658840dddc7894
Jim Hayes <spinaltap12345@gmail.com>
2020-01-11 09:33:28 -0800

    Bump version for issue-2 fix.

--- Pathfinder.js
+++ Pathfinder.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var PATHFINDER_VERSION = '1.4.1.0';
+var PATHFINDER_VERSION = '1.4.1.1';
 
 /*
  * This module loads the rules from the Pathfinder Reference Document.  The
