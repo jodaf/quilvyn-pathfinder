65fb251e9e7350cf1c7be3cbd5c62a3ee97f98c4
Jim Hayes <spinaltap12345@gmail.com>
2022-12-31 19:59:46 -0800

    Clarify Smite Evil note text.

--- Pathfinder.js
+++ Pathfinder.js
@@ -1080,7 +1080,7 @@ Pathfinder.FEATURES = {
          '"+2 Fly/-4 Intimidate/+4 Stealth"',
   'Smite Evil':
     'Section=combat ' +
-    'Note="+%V attack, +%1 damage, bypass DR, and +%2 AC vs. evil foe %3/dy"',
+    'Note="+%V attack, +%1 HP damage, bypass DR, and +%2 AC vs. evil foe (dbl HP vs. outsider, dragon, or undead) %3/dy"',
   'Snatch Arrows':'Section=combat Note="Catch ranged weapons"',
   'Sneak Attack':
     'Section=combat ' +
